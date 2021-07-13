ffea2cafe931b3f47a5f9fe5887afda4a59c2d50
Yohei Sakata <yosakata@amazon.com>
2021-07-13 00:35:13 -0700

    added sample file

--- codeguruSample1.py
+++ codeguruSample1.py
@@ -3,21 +3,27 @@
 import csv
 
 # Case 1
-f = open('csvSample.csv')
+#f = open('csvSample.csv')
+#print("opened file")
+
+with open('csvSample.csv'):
+  print("opened file")
 
 # Case 2
 mycard = '4980 1234 1234 1234'
+print(mycard)
 
 # Case 3
 fruites = ["Apple", "Banana", "Grapes"]
-if len(fruites) > 0:
+#if len(fruites) > 0:
+if fruites:
   print("there are fruites")
 
 # Case 4
 item = {"sakata", "090-1234-1234" }
-if item == None:
+#if item == None:
+if item is None:
   print("Dictionary is empty")
-
 
 #try:
 #  csvfile = csv.reader(f)
